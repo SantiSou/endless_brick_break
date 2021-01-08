8bbbc0724c45ee7bbdad7264ce24d82cddb88cee
PC-SANTIAGO\Santiago <souto.santiago.mattias@gmail.com>
2021-01-07 23:35:51 -0300

    commit - added ball class

--- endless_brick_break.py
+++ endless_brick_break.py
@@ -19,21 +19,25 @@ background = background.convert()
 background.fill((255,255,255))
 
 bricks = Group()
-players = Group()
 brick_x = 0
 brick_y = 0
 brick_width = 30 # Ancho 
 brick_height = 10 # Alto
-
 brick_separation = 1
 amount_bricks = (screen_width / brick_width) - brick_separation
-
 speed_factor = 1
 
+players = Group()
 player_x = 0
 player_y = 0
 player_width = 45
 player_height = 3
+
+balls = Group()
+ball_x = 0
+ball_y = 0
+ball_width = 8
+ball_height = 8
 
 class Player(Sprite):
 
@@ -57,7 +61,36 @@ class Player(Sprite):
         for player in players.sprites():
             player.draw_player() 
 
+class Ball(Sprite):
 
+    def __init__(self,screen,new_player):
+
+        super(Ball, self).__init__()
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+
+        self.ball_rect = pygame.Rect(ball_x,ball_y,ball_width,ball_height)
+
+        self.ball_rect.centery = new_player.player_rect.centery
+        self.ball_rect.centerx = new_player.player_rect.centerx
+
+    def draw_ball(self):
+
+        pygame.draw.rect(self.screen,(0,0,0),self.ball_rect)
+
+    def update(self):
+
+        if self.ball_rect.left < 0:
+            self.ball_rect.centerx += speed_factor
+        elif self.ball_rect.right > screen_width:
+            self.ball_rect.centerx -= speed_factor
+        # self.ball_rect.centerx = 
+
+        for ball in balls.sprites():
+            if len(balls) == 1:
+                ball.draw_ball()
+            else:
+                balls.remove(ball)
 class Brick(Sprite):
 
     def __init__(self,screen):
@@ -67,9 +100,6 @@ class Brick(Sprite):
         self.screen_rect = screen.get_rect()
 
         self.brick_rect = pygame.Rect(brick_x,brick_y,brick_width,brick_height)
-        # self.brick_rect.y = float(self.brick_rect.y)
-        # self.brick_rect.centerx = self.screen_rect.centerx
-        # self.brick_rect.centery = self.screen_rect.centery
 
 
     def draw_brick(self):
@@ -93,14 +123,17 @@ players.add(new_player)
 while True:
 
     clock.tick(60) # Frames Per Second
+
+    
     # Evaluar el valor de clock.tick para utilizarlo de mecanismo de generación de bloques
     for event in pygame.event.get():
-
-        # if event.type == pygame.KEYDOWN:
-            # if event.key == pygame.K_SPACE:
-                       
+                               
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1: # Botón izquierdo del ratón
+                new_ball = Ball(screen,new_player)
+                balls.add(new_ball)
 
     if 1 == random.randint(1,100):
         new_brick = Brick(screen)
@@ -116,6 +149,7 @@ while True:
     speed_factor += 0.001
 
     screen.blit(background, (0, 0))
+    balls.update()
     players.update(mouse_x)
     bricks.update(speed_factor)
     pygame.display.flip()
